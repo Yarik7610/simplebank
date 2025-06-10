64584bf64bb8872f7cfb377a7fcb28f1ebb9b781
Yaroslav Yanovich <arikarik782@gmail.com>
2025-06-10 19:27:54 +0300

    feat: add more account sqlc queries

--- db/queries/account.sql
+++ db/queries/account.sql
@@ -2,3 +2,23 @@
 INSERT INTO accounts (owner, balance, currency)
 VALUES ($1, $2, $3)
 RETURNING *;
+
+-- name: GetAccount :one
+SELECT * FROM accounts
+WHERE id = $1;
+
+-- name: ListAuthors :many
+SELECT * FROM accounts
+ORDER BY id
+FETCH FIRST $1 ROWS ONLY
+OFFSET $2 ROWS;
+
+-- name: UpdateAccount :one
+UPDATE accounts
+SET balance = $2
+WHERE id = $1
+RETURNING *;
+
+-- name: DeleteAccount :exec
+DELETE FROM accounts
+WHERE id = $1;

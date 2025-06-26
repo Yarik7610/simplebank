2f84be8d3035a317df79ff15c96ee7bf03793e51
Yaroslav Yanovich <arikarik782@gmail.com>
2025-06-26 13:19:28 +0300

    feat: add GET list accounts hadler

--- api/account.go
+++ api/account.go
@@ -36,12 +36,12 @@ func (server *Server) createAccount(ctx *gin.Context) {
 	ctx.JSON(http.StatusCreated, account)
 }
 
-type getAccountQuery struct {
+type getAccountRequest struct {
 	ID int64 `uri:"id" binding:"required,min=1"`
 }
 
 func (server *Server) getAccount(ctx *gin.Context) {
-	var req getAccountQuery
+	var req getAccountRequest
 	if err := ctx.ShouldBindUri(&req); err != nil {
 		ctx.JSON(http.StatusBadRequest, errorResponse(err))
 		return
@@ -59,4 +59,30 @@ func (server *Server) getAccount(ctx *gin.Context) {
 	}
 
 	ctx.JSON(http.StatusOK, account)
+}
+
+type listAccountsRequest struct {
+	PageNum  int32 `form:"page_num" binding:"required,min=1"`
+	PageSize int32 `form:"page_size" binding:"required,min=5,max=10"`
+}
+
+func (server *Server) listAccounts(ctx *gin.Context) {
+	var req listAccountsRequest
+	if err := ctx.ShouldBindQuery(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	arg := db.ListAccountsParams{
+		Limit:  req.PageSize,
+		Offset: req.PageSize * (req.PageNum - 1),
+	}
+
+	accounts, err := server.store.ListAccounts(ctx, arg)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	ctx.JSON(http.StatusOK, accounts)
 }
